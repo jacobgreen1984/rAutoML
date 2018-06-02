8451552203cbccaf01633916b84c9a333e390abc
jacobgreen1984 <jacobgreen@github.com>
2018-06-02 14:18:43 +0900

    jacob

--- R/auto_gbm.R
+++ R/auto_gbm.R
@@ -1,5 +1,5 @@
 #' @title autoGBM
-#' @description auto train GBM using pre-defined strategies 
+#' @description auto train GBM using pre-defined strategies
 #' @param data_hex H2ODataFrame
 #' @param x independent variables
 #' @param y dependent variable
@@ -9,18 +9,16 @@
 #' h2o.init()
 #' data(churn, package = "rAutoML")
 #' data_hex <- as.h2o(churn)
-#' split_hex <- h2o.splitFrame(data = data_hex, ratios = c(0.5,0.25), seed = 1234)
+#' split_hex <- h2o.splitFrame(data = data_hex, ratios = c(0.5,0.3), seed = 1234)
 #' train_hex <- split_hex[[1]]
 #' valid_hex <- split_hex[[2]]
-#' test_hex  <- split_hex[[3]]
-#' y <- "Churn."
-#' x <- setdiff(names(data_hex),  c(y))
-#' output <- autoGBM()
+#' test_hex <- split_hex[[3]]
+#' y = "Churn."
+#' x = colnames(data_hex)[colnames(data_hex)!=y]
+#' output <- autoGBM(training_frame=train_hex, validation_frame_valid_hex, x=x, y=y)
 #' @export
-
-# autu_gbm ----
 autoGBM <- function(training_frame, validation_frame, x, y){
-  
+
   # run expriment
   path = 'R/gbm'
   source(file.path(path, 'gbm_w_default.R'))
@@ -32,8 +30,8 @@ autoGBM <- function(training_frame, validation_frame, x, y){
   source(file.path(path, 'gbm_w_bayesian.R'))
   source(file.path(path, 'gbm_w_bayesian_nfolds.R'))
   source(file.path(path, 'gbm_w_cartesian_bayesian_nfolds.R'))
-  
-  # collect model 
+
+  # collect model
   gbm_list <- list(
     gbm_w_default,
     gbm_w_stoppingRules,
@@ -45,14 +43,14 @@ autoGBM <- function(training_frame, validation_frame, x, y){
     gbm_w_bayesian_nfolds,
     gbm_w_cartesian_bayesian_nfolds
   )
-  
+
   # make report
   gbm_report <- data.frame(
     "model" = c("default","stop","cartes","random","cartRan","cartRanCV","bayes","bayesCV","carBayCV"),
     "train" = sapply(gbm_list, function(x) h2o.performance(x, train=TRUE)@metrics$AUC),
     "test" = sapply(gbm_list, function(x) h2o.performance(x, newdata=test_hex)@metrics$AUC)
   )
-  
+
   return(gbm_summary)
 }
 
